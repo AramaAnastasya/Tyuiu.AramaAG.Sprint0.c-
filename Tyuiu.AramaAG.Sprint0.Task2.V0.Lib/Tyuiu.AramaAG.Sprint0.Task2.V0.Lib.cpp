7e495d2e094a02e48c57d01c737e0db99d910f68
AramaAnastasya <Tyuiu.AramaAG@outlook.com>
2023-09-07 15:39:55 +0500

    Task2

---
+++ Tyuiu.AramaAG.Sprint0.Task2.V0.Lib/Tyuiu.AramaAG.Sprint0.Task2.V0.Lib.cpp
@@ -0,0 +1,16 @@
+﻿// Tyuiu.AramaAG.Sprint0.Task2.V0.Lib.cpp : Определяет функции для статической библиотеки.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "..//..//Tyuiu.Cours.cpp/Tyuiu.Cours.cpp.cpp"
+
+// TODO: Это пример библиотечной функции.
+class Service : public ISprint0Task2V0
+{
+	virtual int Add(int a1, int a2) override
+	{
+		return a1 + a2;
+	}
+
+};
---
+++ Tyuiu.AramaAG.Sprint0.Task2.V0.Test/Tyuiu.AramaAG.Sprint0.Task2.V0.Test.cpp
@@ -0,0 +1,31 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Tyuiu.AramaAG.Sprint0.Task2.V0.Lib/Tyuiu.AramaAG.Sprint0.Task2.V0.Lib.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			ISprint0Task2V0* service = new Service();
+			int a1 = 11;
+			int a2 = 9;
+			int c;
+			c = service->Add(a1, a2);
+			Assert::AreEqual(20, a1 + a2);
+		}
+
+		TEST_METHOD(TestMethod2)
+		{
+			ISprint0Task2V0* service = new Service();
+			int a1 = 13;
+			int a2 = 7;
+			int c = service->Add(a1, a2);
+			Assert::AreEqual(20, a1 + a2);
+		}
+	};
+}
